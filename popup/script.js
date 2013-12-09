115f2ab24ccf2f0a65c3c4c585a472f776d68508
Tony Li <tonyli.lives@gmail.com>
2013-12-09 03:28:20 -0800

    fixing overlay condition

--- popup/script.js
+++ popup/script.js
@@ -1,35 +1,22 @@
 $(document).ready(function(){
     var current = parseInt($.cookie('satire_edition'), 10);
-    console.log('==================logging==================');
-    console.log('before', current);
     if (!current) {
         current = 0;
     }
 
-    if ($.cookie('satire_edition') == '2') {
-        show();
-        $.cookie('satire_edition', 3, { expires: 1 });
-        return;
-    }
+    //console.log('satire cookie before', current);
+    var next = show(current);
+    $.cookie('satire_edition', next, { expires: 1 });
+    //console.log('satire cookie after', $.cookie('satire_edition'));
 
-    if ($.cookie('satire_edition') == '1') {
-        $.cookie('satire_edition', 2, { expires: 1 });
-        return;
-    }
-
-    if (!$.cookie('satire_edition')) {
-        show();
-        $.cookie('satire_edition', 1, { expires: 1 });
-        return;
-    }
-
-    console.log('after', $.cookie('satire_edition'));
-
-    function show() {
-        $.get('http://ubyssey.ca/wp-content/themes/ubyssey2012/popup/content.html', function(data) {
-            var height = $(document).height();
-            $('.satire-overlay').css('height', height);
-            $('.satire-overlay').html(data).show();
-        });
+    function show(num) {
+        if (num === 0) {
+            $.get('http://ubyssey.ca/wp-content/themes/ubyssey2012/popup/content.html', function(data) {
+                var height = $(document).height();
+                $('.satire-overlay').css('height', height);
+                $('.satire-overlay').html(data).show();
+            });
+        }
+        return num + 1;
     }
 });
