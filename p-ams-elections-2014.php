6c7a164b34befa4b72791cd0cefc40d47f3d69e1
Tony Li <tonyli.lives@gmail.com>
2014-02-10 22:31:15 -0800

    removed feature box on ams 2014

--- p-ams-elections-2014.php
+++ p-ams-elections-2014.php
@@ -25,85 +25,38 @@ get_header('ams-elections-2014'); ?>
         <div class="section-header">
             <h1 class="elections">The Stories</h1>
         </div>
-        <div class="featured-slider resp hide-mobile">
-            <?php
+        <div class="small-feed recent-feed">
+            <ul class="recent-feed">
+                <?php
                 // Get most recent news, opinion, or features articles with the ams-elections category
                 $args = array(
                     'post_type' => array('news', 'opinion', 'features', 'ams'),
                     'year' => 2014,
-                    'posts_per_page' => '3',
+                    'posts_per_page' => '10',
                     'tax_query' => array(
                         'relation' => 'OR',
                         array(
                             'taxonomy' => 'news_category',
                             'field' => 'slug',
-                            'terms' => 'ams-elections-featured'
+                            'terms' => array('ams-elections', 'ams-elections-featured')
                         ),
                         array(
                             'taxonomy' => 'opinion_category',
                             'field' => 'slug',
-                            'terms' => 'ams-elections-featured'
+                            'terms' => array('ams-elections', 'ams-elections-featured')
                         ),
                         array(
                             'taxonomy' => 'feature_category',
                             'field' => 'slug',
-                            'terms' => 'ams-elections-featured'
+                            'terms' => array('ams-elections', 'ams-elections-featured')
                         ),
                         array(
                             'taxonomy' => 'ams_category',
                             'field' => 'slug',
-                            'terms' => 'ams-elections-featured'
+                            'terms' => array('ams-elections', 'ams-elections-featured')
                         )
                     )
                 );
-                $query = new WP_Query( $args );
-                $the_query = clone $query;
-
-                $count = 1;
-                $zindex = '';
-
-                while ( $the_query->have_posts() ) : $the_query->the_post();
-                    if( $count === 1 ){ $zindex = 'style="z-index: 2;"'; }
-                    else { $zindex = 'style="z-index: 1;"'; }
-                    $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'featured-slider' );
-                    echo '<img src="' . $image[0] . '" alt="" class="resp slide" id="slide-' . $count . '"' . $zindex . '>';
-                    $count = $count + 1;
-                endwhile;
-                wp_reset_postdata(); ?>
-
-                <div class="headlines">
-                    <?php
-                    // Get most recent news, opinion, or features articles with the ams-elections category
-                    $the_query = clone $query;
-
-                    $count = 1;
-                    $child = '';
-                    while ( $the_query->have_posts() ) : $the_query->the_post();
-                        if( $count === 1 ){ $child = 'child-left active'; }
-                        if( $count === 2 ){ $child = 'child-center'; }
-                        if( $count === 3 ){ $child = 'child-right'; }
-                        echo '<div class="headline floatLeft ' . $child . '"><a href="' . get_post_meta( get_the_ID(), 'url', true) . '" class="headline-hover" id="' . $count . '"><h3>' . get_the_title() . '</h3></a></div>';
-                        $count = $count + 1;
-                    endwhile;
-                    wp_reset_postdata(); ?>
-                </div>
-        </div><!-- featured-slider -->
-
-        <div class="small-feed recent-feed">
-            <ul class="recent-feed">
-                <?php
-                // Get most recent news, opinion, or features articles with the ams-elections category
-                $the_query = clone $query;
-                $to_exclude = array();
-                foreach($the_query->posts as $exclude) {
-                    $to_exclude[] = $exclude->ID;
-                }
-                $args['post__not_in'] = $to_exclude;
-                $args['posts_per_page'] = 10;
-                $args['tax_query'][0]['terms'] = array('ams-elections', 'ams-elections-featured');
-                $args['tax_query'][1]['terms'] = array('ams-elections', 'ams-elections-featured');
-                $args['tax_query'][2]['terms'] = array('ams-elections', 'ams-elections-featured');
-                $args['tax_query'][3]['terms'] = array('ams-elections', 'ams-elections-featured');
                 $the_query = new WP_Query( $args );
 
                 // The Loop
