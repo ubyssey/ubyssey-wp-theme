bd7bb2a4228e89e66cf96ca1a33a84c2534e415b
Peter Siemens <peterjsiemens@gmail.com>
2014-06-10 22:55:29 -0600

    fixed advertisements

--- header.php
+++ header.php
@@ -30,15 +30,41 @@
     <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.0/jquery.min.js"></script>
 
     <script type="text/javascript">
-      var _gaq = _gaq || [];
-      _gaq.push(['_setAccount', 'UA-2313592-2']);
-      _gaq.push(['_trackPageview']);
 
-      (function() {
-        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-        ga.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'stats.g.doubleclick.net/dc.js';
-        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-      })();
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+        ga('create', 'UA-2313592-2', 'ubyssey.ca');
+        ga('send', 'pageview');
+        
+    </script>
+
+    <script type='text/javascript'>
+        var googletag = googletag || {};
+        googletag.cmd = googletag.cmd || [];
+        (function() {
+        var gads = document.createElement('script');
+        gads.async = true;
+        gads.type = 'text/javascript';
+        var useSSL = 'https:' == document.location.protocol;
+        gads.src = (useSSL ? 'https:' : 'http:') + 
+        '//www.googletagservices.com/tag/js/gpt.js';
+        var node = document.getElementsByTagName('script')[0];
+        node.parentNode.insertBefore(gads, node);
+        })();
+    </script>
+
+    <script type='text/javascript'>
+        googletag.cmd.push(function() {
+        googletag.defineSlot('/61222807/News_BoxA_300x250', [300, 250], 'div-gpt-ad-1400542176207-8').addService(googletag.pubads());
+        googletag.defineSlot('/61222807/News_BoxB_300x250', [300, 250], 'div-gpt-ad-1400542176207-9').addService(googletag.pubads());
+        googletag.defineSlot('/61222807/News_Inline_468x60', [468, 60], 'div-gpt-ad-1401420814378-10').addService(googletag.pubads());
+        googletag.defineSlot('/61222807/News_leaderboard_728x90', [728, 90], 'div-gpt-ad-1400542176207-11').addService(googletag.pubads());
+        //googletag.pubads().enableSingleRequest();
+        googletag.enableServices();
+        });
     </script>
 
     <?php wp_head(); ?>
@@ -48,7 +74,7 @@
     <script type="text/javascript" src="//platform.twitter.com/widgets.js"></script>
     <!-- fitvid - responsive videos -->
     <script src="<?php bloginfo( 'template_url' ); ?>/js/jquery.fitvids-ck.js"></script>
-    <script src="<?php bloginfo( 'template_url' ); ?>/js/dfp.js"></script>
+    
 </head>
 
 <body <?php body_class(); ?>>
